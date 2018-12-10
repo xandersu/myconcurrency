43222b1da27bb3e784cb7ecdfb78823e8693eb80
suxun <suxun@gotokeep.com>
2018-12-10 21:13:14 +0800

    Initial commit

---
+++ src/main/java/com/xandersu/myconcurrency/MyconcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.xandersu.myconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyconcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyconcurrencyApplication.class, args);
+    }
+}
