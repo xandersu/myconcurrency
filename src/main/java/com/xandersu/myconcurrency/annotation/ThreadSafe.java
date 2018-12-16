b5a2f436e2d6cba850375e6309013f23f569af73
xandersu <840595532@qq.com>
2018-12-16 16:28:03 +0800

    C3-p2 课程准备

---
+++ src/main/java/com/xandersu/myconcurrency/annotation/ThreadSafe.java
@@ -0,0 +1,21 @@
+package com.xandersu.myconcurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author: suxun
+ * @Date: 2018/12/16 16:21
+ * @Description: 课程里标记线程安全的类或者写法
+ */
+@Target(ElementType.TYPE)   //注解作用的目标
+//注解作用范围
+//runtime 注解会在字节码中存在，运行时通过反射拿到
+//source 编译时忽略掉
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+
+    String value() default "";
+}
--- src/main/java/com/xandersu/myconcurrency/concurrencybasic02/CPUMultiLevelCacheCacheConsistency01.java
+++ src/main/java/com/xandersu/myconcurrency/concurrencybasic02/CPUMultiLevelCacheCacheConsistency01.java
@@ -6,7 +6,7 @@ package com.xandersu.myconcurrency.concurrencybasic02;
  * @Description: CPU多级缓存-缓存一致性
  * CPU多级缓存：
  * 为什么需要CPU cache：CPU频率太快了，主存跟不上，这样在处理器时钟周期内，cpu常常需要等待主存，浪费资源。
- * 所以cache出现，是为了缓解CPU和内存之间速度的不匹配问题（cpu-》cache=》memory）
+ * 所以cache出现，是为了缓解CPU和内存之间速度的不匹配问题（cpu -> cache -> memory）
  * CPU cache 意义：
  * 1）时间局部性：如果某个数据被访问，在不久的将来很可能再次被访问
  * 2）空间局部性：如果某个数据被访问，那么与他相邻的数据很快也可能被访问
