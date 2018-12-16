8aa638bcad858d3b060339c03131d1b6b7520edc
xandersu <840595532@qq.com>
2018-12-16 16:06:15 +0800

    C2-S4 并发好处坏处 总结

---
+++ src/main/java/com/xandersu/myconcurrency/concurrencybasic02/ConcurrencyAdvantagesAndRisks04.java
@@ -0,0 +1,16 @@
+package com.xandersu.myconcurrency.concurrencybasic02;
+
+/**
+ * @Author: suxun
+ * @Date: 2018/12/16 14:41
+ * @Description: 并发的优势和风险
+ * 速度：
+ * 设计：
+ * 资源利用
+ * ·
+ * 安全性
+ * 活跃性
+ * 性能
+ */
+public interface ConcurrencyAdvantagesAndRisks04 {
+}
---
+++ src/main/java/com/xandersu/myconcurrency/concurrencybasic02/Summary.java
@@ -0,0 +1,12 @@
+package com.xandersu.myconcurrency.concurrencybasic02;
+
+/**
+ * @Author: suxun
+ * @Date: 2018/12/16 14:45
+ * @Description: 总结
+ * cpu多级缓存：缓存一致性、乱序执行优化
+ * java内存模型：jmm规定，抽象结构，同步八种操作以及规则
+ * java并发的优势和风险
+ */
+public interface Summary {
+}
